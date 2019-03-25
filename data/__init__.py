f4b0a642acceef50d31b1c2a24beee543abbfbce
sean85914 <sean19960914@gmail.com>
2019-03-25 18:04:06 +0800

    for strawberry dataset

--- data/__init__.py
+++ data/__init__.py
@@ -1,7 +1,9 @@
 from .voc0712 import VOCDetection, VOCAnnotationTransform, VOC_CLASSES, VOC_ROOT
 
 from .coco import COCODetection, COCOAnnotationTransform, COCO_CLASSES, COCO_ROOT, get_label_map
+from .strawberry import StrawberryDetection, strawberryAnnotationTransform, STRAWBERRY_CLASSES, STRAWBERRY_ROOT
 from .config import *
+
 import torch
 import cv2
 import numpy as np
--- data/config.py
+++ data/config.py
@@ -29,7 +29,7 @@ voc = {
 coco = {
     'num_classes': 201,
     'lr_steps': (280000, 360000, 400000),
-    'max_iter': 400000,
+    'max_iter': 100000, #400000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
@@ -39,4 +39,19 @@ coco = {
     'variance': [0.1, 0.2],
     'clip': True,
     'name': 'COCO',
+}
+
+strawberry = {
+    'num_classes': 2,
+    'lr_steps': (10000, 50000, 80000),
+    'max_iter': 100000, #400000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [21, 45, 99, 153, 207, 261],
+    'max_sizes': [45, 99, 153, 207, 261, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'strawberry',
 }
